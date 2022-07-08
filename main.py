94dbe26ea2bb5f5dde9c7f3711d01f4abed4eaae
appletime81 <itrimsl8696@gmail.com>
2022-07-09 07:25:38 +0800

    Update

--- main.py
+++ main.py
@@ -49,6 +49,7 @@ def table_1(content):  # content = lines
     global table_1_dict
     PRODUCTION_COUNT_FLAG = False
     FORM_ALARM_ITEM_FLAG = False
+
     for line in content:
         # get Package name
         if re.search("Package\W+:\W+", line):
@@ -60,7 +61,15 @@ def table_1(content):  # content = lines
 
         # get Lot Started
         if re.search("Lot Started\W+:\W+", line):
-            table_1_dict["Lot Started"].append(line.split(":")[1][1:])
+            table_1_dict["Lot Started"].append(
+                re.search(r"\d+/\d+/\d+ \d+:\d+:\d+", line).group()
+            )
+
+        # get Lot Finished
+        if re.search("Lot Finished\W+:\W+", line):
+            table_1_dict["Lot Finished"].append(
+                re.search(r"\d+/\d+/\d+ \d+:\d+:\d+", line).group()
+            )
 
         # set PRODUCTION COUNT flag if find string of 'PRODUCTION COUNT'
         if re.search("PRODUCTION COUNT", line):
@@ -75,122 +84,122 @@ def table_1(content):  # content = lines
             for i in range(len(line.split(" "))):
                 if line.split(" ")[i] != "" and line.split(" ")[i] != "FORM":
                     temp_items_list.append(line.split(" ")[i])
-            for key, value in zip(TABLE_1_COL_NAMES[5:9], temp_items_list):
+            for key, value in zip(TABLE_1_COL_NAMES[4:9], temp_items_list):
                 table_1_dict[key].append(value)
 
             # close PRODUCTION COUNT Flag
             PRODUCTION_COUNT_FLAG = False
 
         if FORM_ALARM_ITEM_FLAG and re.search("Light Alarm", line):
-            table_1_dict["Light Alarm"] = [
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0]
+            table_1_dict["Light Alarm"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Package Type", line):
-            table_1_dict["Package Type"] = [
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0]
+            table_1_dict["Package Type"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Package Size", line):
-            table_1_dict["Package Size"] = [
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0]
+            table_1_dict["Package Size"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Package Absence", line):
-            table_1_dict["Package Absence"] = [
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0]
+            table_1_dict["Package Absence"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Lead Count", line):
-            table_1_dict["Lead Count"] = [
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0]
+            table_1_dict["Lead Count"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Broken Lead", line):
-            table_1_dict["Broken Lead"] = [
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0]
+            table_1_dict["Broken Lead"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Bent Lead", line):
-            table_1_dict["Bent Lead"] = [
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0]
+            table_1_dict["Bent Lead"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Intrusion", line):
-            table_1_dict["Intrusion"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Intrusion"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Protrusion", line):
-            table_1_dict["Protrusion"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Protrusion"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Lead Span", line):
-            table_1_dict["Lead Span"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Lead Span"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Terminal Dimension", line):
-            table_1_dict["Terminal Dimension"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Terminal Dimension"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Chip Out", line):
-            table_1_dict["Chip Out"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Chip Out"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Mark Count", line):
-            table_1_dict["Mark Count"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Mark Count"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("No Mark", line):
-            table_1_dict["No Mark"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["No Mark"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Mark Offset", line):
-            table_1_dict["Mark Offset"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Mark Offset"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Wrong Char", line):
-            table_1_dict["Wrong Char"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Wrong Char"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Broken Char", line):
-            table_1_dict["Broken Char"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Broken Char"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Residue Char", line):
-            table_1_dict["Residue Char"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Residue Char"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Missing Ref", line):
-            table_1_dict["Missing Ref"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Missing Ref"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Mold Cont", line):
-            table_1_dict["Mold Cont"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Mold Cont"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
         if FORM_ALARM_ITEM_FLAG and re.search("Shift Cut", line):
-            table_1_dict["Shift Cut"].append([
-                num for num in line.split(" ") if num.replace(".", "").isdigit()
-            ][0])
+            table_1_dict["Shift Cut"].append(
+                [num for num in line.split(" ") if num.replace(".", "").isdigit()][0]
+            )
 
             # close FORM ALARM ITEM FLAG
             FORM_ALARM_ITEM_FLAG = False
 
 
-def table_2(content):  # content = lines
+def table_2(content):-  # content = lines
     pass
 
 
@@ -201,6 +210,7 @@ def table_3(content):  # content = lines
 if __name__ == "__main__":
     txt_files = get_all_txtx_files(os.getcwd())
     for txt_file in txt_files:
+        # print(txt_file)
         with open(txt_file, "r") as f:
             lines = f.readlines()
         table_1(lines)
